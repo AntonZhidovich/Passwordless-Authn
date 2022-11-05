432efdffc8b5b3b5e9b3c2b8dd40b763c2218e41
Антон Жидович <toni.zhidovich@mail.ru>
2022-11-05 21:22:56 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Passwordless_Authn.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("Home/Index"), Route("/")]
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+    }
+}
