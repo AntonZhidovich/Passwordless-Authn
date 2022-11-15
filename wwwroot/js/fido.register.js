3dae353bc8d5453759244f5cd325be0559c0ee5a
Anton Zhidovich <toni.zhidovich@mail.ru>
2022-11-15 23:04:21 +0300

    removed redirection after fido registration

--- wwwroot/js/fido.register.js
+++ wwwroot/js/fido.register.js
@@ -64,7 +64,6 @@ async function handleRegisterSubmit(event) {
 
     try {
         response = await sendResponce(encCreds);
-        window.location.href = "/";
     }
     catch (e) {
         console.log(e);
